4c1b9be01c7de1b606132997910989093d6c8a97
MichaelNGAN1201 <119638972+MichaelNGAN1201@users.noreply.github.com>
2023-02-10 17:23:50 -0700

    Update Refrigerators.cs
    adding comments

--- OOB_Assignment1(Final Copy)/OOB_Assignment1/OOB_Assignment1/Refrigerators.cs
+++ OOB_Assignment1(Final Copy)/OOB_Assignment1/OOB_Assignment1/Refrigerators.cs
@@ -6,19 +6,22 @@ using System.Threading.Tasks;
 
 namespace OOB_Assignment1
 {
+    //create class Refrigerators and make it inherited from the class Appliance
     class Refrigerators : Appliance
     {
 
+        // create attributes for class Appliance
         private int numDoor;
         private int height;
         private int width;
 
 
+        //create public attributes to access the private attributes
         public int NumDoor { get { return numDoor; } set { numDoor = value; } }
         public int Height { get { return height; } set { height = value; } }
         public int Width { get { return width; } set { width = value; } }
 
-
+        //Construct
         public Refrigerators() 
         {
             NumDoor = 0;
@@ -26,6 +29,7 @@ namespace OOB_Assignment1
             Width = 0;
         }
 
+        //create method to change the attributes' type from int to string for displaying information
         public override string ToString()
         {
             string NumDoorString = string.Empty;
@@ -51,9 +55,5 @@ namespace OOB_Assignment1
                 $"Height: {Height}\n" +
                 $"Width: {Width}\n";
         }
-
-
-
-
     }
 }
