a77b3e07a11916cc7e86d8c9ba4f3613a31b4b7c
MichaelNGAN1201 <119638972+MichaelNGAN1201@users.noreply.github.com>
2023-02-10 17:27:18 -0700

    Update Vacuums.cs
    add comment

--- OOB_Assignment1(Final Copy)/OOB_Assignment1/OOB_Assignment1/Vacuums.cs
+++ OOB_Assignment1(Final Copy)/OOB_Assignment1/OOB_Assignment1/Vacuums.cs
@@ -6,14 +6,18 @@ using System.Threading.Tasks;
 
 namespace OOB_Assignment1
 {
+    //create class Vaccums and make it inherited from class Appliance
     class Vacuums : Appliance
     {
+        //create attributes for class Vaccums
         private string grade;
         private int batteryVottage;
 
+        //create public attributes to access private attributes
         public string Grade { get { return grade; } set { grade = value; } }
         public int BatteryVottage { get { return batteryVottage; } set { batteryVottage = value; } }
-
+    
+        //Construct
         public Vacuums() 
         {
             Grade = string.Empty;
@@ -21,6 +25,7 @@ namespace OOB_Assignment1
 
         }
 
+        //create method to change the attributes type from int to String
         public override string ToString()
         {
             string BatteryVottageString = string.Empty;
@@ -36,10 +41,5 @@ namespace OOB_Assignment1
                 $"Grade: {Grade}\n" +
                 $"Battery Voltage: {BatteryVottageString}\n";
         }
-
-
-
-
-
     }
 }
