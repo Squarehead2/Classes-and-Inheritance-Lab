11ae994ec09d56f43f8d5cad1109fcd5a65d5416
MichaelNGAN1201 <119638972+MichaelNGAN1201@users.noreply.github.com>
2023-02-10 17:31:09 -0700

    Update Microwaves.cs
    add comments

--- OOB_Assignment1(Final Copy)/OOB_Assignment1/OOB_Assignment1/Microwaves.cs
+++ OOB_Assignment1(Final Copy)/OOB_Assignment1/OOB_Assignment1/Microwaves.cs
@@ -6,20 +6,25 @@ using System.Threading.Tasks;
 
 namespace OOB_Assignment1
 {
+    // create class Microwaves and make it inherited from class Appliance
     class Microwaves : Appliance
     {
+        // create attributes for class Microwaves
         private double capacity;
         private char roomType;
 
+        // create public attributes to access private attributes
         public double Capacity { get { return capacity; } set { capacity = value; } }
         public char RoomType { get { return roomType; } set { roomType = value; } }
 
+        //Construct
         public Microwaves() 
         {
             Capacity = 0;
             RoomType = char.MinValue;
         }
 
+        // create method to change the attributes' type from char/double to string
         public override string ToString()
         {
             bool result;
@@ -32,13 +37,10 @@ namespace OOB_Assignment1
             {
                 RoomTypeString = "Work Site";
             }
-
             
             return base.ToString()+
                 $"Capacity: {Capacity}\n" +
                 $"Room Type:{RoomTypeString}\n" ;
         }
-
-
     }
 }
